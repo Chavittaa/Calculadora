739c395143e3a3a11f03d59cea00442c2c67b4a7
Chavittaa <saagoba@gmail.com>
2016-09-04 11:38:57 -0500

    Primer commit desde ubuntu

--- app/src/main/java/com/edu/itq/chavitta/calculadora/MainActivity.java
+++ app/src/main/java/com/edu/itq/chavitta/calculadora/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+//primer commit desde ubuntu!
 public class MainActivity extends AppCompatActivity {
 
     boolean band = true;
